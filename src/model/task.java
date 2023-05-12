70f5e5d3372d3e7de6ca76038425d55f1f12e78b
USER <USER@DESKTOP-R0KPOQ0>
2023-05-12 17:24:52 +0900

    テーブル名の修正

--- src/model/task.java
+++ src/model/task.java
@@ -12,7 +12,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "task")
+@Table(name = "tasks")
 @NamedQueries({
     @NamedQuery(
             name = "getAlltask",
